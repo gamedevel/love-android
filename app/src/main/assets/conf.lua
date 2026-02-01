2facdeed75c82a534d03a1b0e66352eacd6ea527
gamedevel <y18440105@gmail.com>
2026-02-01 15:38:41 +0630

    Add files via upload

---
+++ app/src/main/assets/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.window.width = 360
+    t.window.height = 640
+    t.window.display = 1
+    t.window.title = "Floppy Man"
+    t.modules.joystick = false
+    -- ဒေါင်လိုက် screen သတ်မှတ်ချက်
+    t.window.orientation = "portrait"
+end
